a3a39abee34507e7a357c20e2c973846d346947d
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2024-05-09 21:39:58 -0500

    Updated specifications checker

--- specifications_checker.sql
+++ specifications_checker.sql
@@ -6,6 +6,13 @@ FROM ourvle.account
 JOIN ourvle.user ON user.UserId = account.UserId
 WHERE account.AccType = 'Student';
 
+
+-- Total number of Courses
+
+SELECT COUNT(*) AS num_courses
+FROM ourvle.course;
+
+
 -- Check number of members for each course
 
 SELECT CourseId, COUNT(*) AS num_members
@@ -13,7 +20,7 @@ FROM ourvle.membership
 GROUP BY CourseId;
 
 
--- Check if All Students have courses between 3 - 6 
+-- Check if All Students have courses with a maximum of 6 courses
 
 SELECT m.UserId, a.AccType, COUNT(m.CourseId) AS num_courses
 FROM ourvle.membership AS m
@@ -24,7 +31,30 @@ GROUP BY m.UserId
 ORDER BY num_courses DESC;
 
 
--- Check if All Lecturers/Course Maintainers teach courses between 1 - 5
+-- Check if All Students have courses minimum 3 courses
+
+SELECT m.UserId, a.AccType, COUNT(m.CourseId) AS num_courses
+FROM ourvle.membership AS m
+JOIN ourvle.user AS u ON m.UserId = u.UserId
+JOIN ourvle.account AS a ON u.UserId = a.UserId
+WHERE a.AccType = 'Student'
+GROUP BY m.UserId
+ORDER BY num_courses;
+
+
+-- Check if All Lecturers/Course Maintainers teach courses at least 1 course
+
+SELECT m.UserId, a.AccType, COUNT(m.CourseId) AS num_courses
+FROM ourvle.membership AS m
+JOIN ourvle.user AS u ON m.UserId = u.UserId
+JOIN ourvle.account AS a ON u.UserId = a.UserId
+WHERE a.AccType = 'Course Maintainer'
+GROUP BY m.UserId
+ORDER BY num_courses;
+
+
+
+-- Check if All Lecturers/Course Maintainers teach courses max 5 courses
 
 SELECT m.UserId, a.AccType, COUNT(m.CourseId) AS num_courses
 FROM ourvle.membership AS m
